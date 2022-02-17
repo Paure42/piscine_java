6eb9f62dbf5ccf8787d923acd7d7b4146f8ede58
Paure <paure@student.42lyon.fr>
2022-02-17 13:01:12 +0100

    day00 - ex01

--- day00/src/ex01/Program.java
+++ day00/src/ex01/Program.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        if (args.length < 1)
-            System.exit(0);
-       int number = (args[0]);
-        System.out.println(number);
+        Scanner scan = new Scanner(System.in);
+        if (scan.hasNext())
+        {
+            int number = scan.nextInt();
+            int iter = 1;
+            boolean isprime = true;
+            if (number < 2)
+            {
+                System.err.print("Illegal argument\n");
+                scan.close();
+                System.exit(-1);
+            }
+            for (int div = 2; div < number; div+=2)
+            {
+                if ((number % div) == 0)
+                {
+                    isprime = false;
+                    break;
+                }
+                iter++;
+            }
+            System.out.printf("%s %d\n", isprime, iter);
+        }
+        scan.close();
+        System.exit(0);
     }
 }
