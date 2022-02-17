f0387187982a9c719fd4b32a00ab1343498a800d
Paure <paure@student.42lyon.fr>
2022-02-17 11:49:27 +0100

    day00 - ex00

---
+++ day00/src/ex01/Program.java
@@ -0,0 +1,8 @@
+public class Program {
+    public static void main(String[] args) {
+        if (args.length < 1)
+            System.exit(0);
+       int number = (args[0]);
+        System.out.println(number);
+    }
+}
