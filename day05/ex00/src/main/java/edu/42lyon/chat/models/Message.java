490035294141750b8f783585b32e000d925884d6
Paure <paure@student.42lyon.fr>
2022-02-18 16:55:34 +0100

    day05 - ex00 models written

---
+++ day05/ex00/src/main/java/edu/42lyon/chat/models/Message.java
@@ -0,0 +1,93 @@
+package edu.42lyon.chat.models;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import user.User;
+
+
+/**
+ * Message
+ */
+public class Message {
+
+    public Message(int id,
+                   User author,
+                   Chatroom room,
+                   String text,
+                   LocalDateTime messageDateTime) {
+        this.id = id;
+        this.author = author;
+        this.room = room;
+        this.text = text;
+        this.messageDateTime = messageDateTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public User getAuthor() {
+        return author;
+    }
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+    public Chatroom getRoom() {
+        return room;
+    }
+    public void setRoom(Chatroom room) {
+        this.room = room;
+    }
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+    public LocalDateTime getMessageDateTime() {
+        return messageDateTime;
+    }
+    public void setMessageDateTime(LocalDateTime messageDateTime) {
+        this.messageDateTime = messageDateTime;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Message))
+            return false;
+        Message c = (Message)obj;
+
+        return (id == c.id &&
+                author == c.author &&
+                room == c.room &&
+                text.equals(c.text) &&
+                messageDateTime == c.messageDateTime
+                );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, author, room, text, messageDateTime);
+    }
+
+    @Override
+    public String toString() {
+        return ("Message{" +
+                "id=" + id +
+                ", author=" + author +
+                ", room=" + room +
+                ", text='" + text + '\'' +
+                ", messageDateTime=" + messageDateTime);
+    }
+
+    private int id;
+    private User author;
+    private Chatroom room;
+    private String text;
+    private LocalDateTime messageDateTime;
+}
